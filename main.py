18278c783255f1edf46f4d6c1bf3a3bd8db5e8db
이승준 <dltmdwns3462@naver.com>
2021-11-18 10:06:35 +0900

    NLP 전처리(토큰화, 불용어, 길이1, (')포함) 완료

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 import pprint
 import requests
 from bs4 import BeautifulSoup
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
 
 ###################
 # 1. 미드 대본 수집 #
@@ -13,6 +17,7 @@ doc = BeautifulSoup(result.text, 'html.parser')
 
 sentence_list = doc.select('p > font')
 
+words = [] # 영단어장
 for i, sentence in enumerate(sentence_list):
     print('=========================================================')
     sentence_txt = sentence.get_text().strip()
@@ -20,11 +25,43 @@ for i, sentence in enumerate(sentence_list):
     clean_sentence = sentence_txt[start_idx+2:]
     pprint.pprint('1 > {}'.format(clean_sentence))
 
+    #######################
+    # 2. 단어만 추출(전처리) #
+    #######################
+    # conda install -c anaconda nltk
+    # 자연어 처리 => NLTK
+    # 2-1. 토큰화(Tokenization)
+    token_list = nltk.word_tokenize(clean_sentence)
+    print('2 >> {}'.format(token_list))
 
+    # 2-2.불용어(StopWord) 제거
+    stopwords = nltk.corpus.stopwords.words('english')
+    stopwords.append(',')
+    stopwords.append('?')
+    stopwords.append('...')
 
-#######################
-# 2. 단어만 추출(전처리) #
-#######################
+    clean_list = []
+    for token in token_list:
+        if token not in stopwords:
+            clean_list.append(token)
+    print('3 >>> {}'.format(clean_list))
+
+    # 2-3. Length 1 이하인 token 제거
+    # len_filter_list = []
+    # for token in clean_list:
+    #     if len(token) > 1:
+    #         len_filter_list.append(token)
+
+    # lambda식을 활용한 Code
+    len_filter_list = list(filter(lambda x: len(x) > 1, clean_list))
+    print('4 >>>> {}'.format(len_filter_list))
+
+    # 2-4.(')포함 된 Toekn 제거
+    clean_filter_list = list(filter(lambda x: "'" not in x, len_filter_list))
+    print('5 >>>>> {}'.format(clean_filter_list))
+    words.extend(clean_filter_list)
+
+print(words)
 
 ###############################
 # 3. 빈도수 순으로 나열 및 시각화 #
