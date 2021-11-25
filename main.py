d2fc243edf2ebeed1eee7551d1f65fcd727e422f
이승준 <dltmdwns3462@naver.com>
2021-11-25 10:13:12 +0900

    단어장 완성 및 Excel 파일 출력

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # MAIN PROGRAM
 
+import time
 import pprint
+
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import nltk
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -82,10 +88,39 @@ text = nltk.Text(words, name ='NMSC') # 중복 제거 전 단어장 입력으로
 #####################################
 # 4. 다음 영어사전 단어정보 수집 및 매칭 #
 #####################################
+def get_dict(word):
+    time.sleep(0.5)
+    url = 'https://dic.daum.net/search.do?q={}'.format(word)
 
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, 'html.parser')
+    meaning_list = doc.select('div.cleanword_type ul.list_search > li')
+
+    # 해당 word의 뜻이 없는 경우! return
+    if len(meaning_list) < 1:
+        return
+
+    word_mean_list = [word]
+
+    for mean in meaning_list:
+        word_mean = mean.select('span')[-1].get_text()
+        word_mean_list.append(word_mean)
+
+    return word_mean_list
+
+total_dict_list = [] # 전체 [[단어1, 뜻1, 뜻2, 뜻3, 뜻4, 뜻5], [단어2, 뜻1, 뜻2, 뜻3, 뜻4, 뜻5], ...]
+for word in unique_words:
+    word_mean_list = get_dict(word) # [word, 뜻1, 뜻2, 뜻3, 뜻4, 뜻5]
+    if word_mean_list != None:
+        total_dict_list.append(word_mean_list)
+
+pprint.pprint(total_dict_list)
 ##################
 # 5. Excel로 저장 #
 ##################
+col_names = ['word', 'mean_1', 'mean_2', 'mean_3', 'mean_4', 'mean_5']
+df_dict = pd.DataFrame(total_dict_list, columns=col_names)
+df_dict.to_excel('words.xlsx', index=False)
 
 #######################
 # 6. 시각화(WordCloud) #
